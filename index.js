2c0c0455b06642bddcda605e76ba46c7376cb9a7
Emily Reuman <reuman@gmail.com>
2017-11-15 06:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = 'Emily Reuman'
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
